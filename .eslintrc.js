663032c6b53a1d58c8ec0f7b3107adf2fdf149f9
Haley Kalb <haley.kalb@gmail.com>
2017-05-19 11:06:48 -0600

    added lint file

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    'ryansobol/browser',
+    'ryansobol/jquery',
+    'ryansobol/es6'
+  ],
+  rules: {
+    "semi": ["error", "never"],
+    "no-unreachable": "error",
+    "no-unexpected-multiline": "error",
+    "strict": "off",
+    "no-console": "off",
+    "no-implicit-globals": "off",
+    "quotes": ["error", "single", { "allowTemplateLiterals": true }],
+    "no-unused-vars": "off",
+    "no-else-return": "off",
+    "no-multiple-empty-lines": "error",
+    "lines-around-comment": "off",
+    "func-style": "off",
+    "dot-notation": "off",
+    "arrow-parens": "off",
+    "object-shorthand": "off",
+    "prefer-arrow-callback": "off",
+    "newline-after-var": "off",
+    "camelcase": "off",
+    "no-use-before-define": "off",
+    "prefer-template": "off",
+    "brace-style": ["error", "1tbs"],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "prefer-const": "off",
+    "max-statements": "off",
+    "new-cap": "off",
+    "max-len": "off",
+    "max-params": "off",
+    "consistent-this": "off",
+    "linebreak-style": "off",
+    "quotes": "off"
+  },
+  "globals": {
+    "chai": true
+  },
+  "env": {
+    "node": true,
+    "mocha": true,
+  }
+};
