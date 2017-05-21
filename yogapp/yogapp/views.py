7447f0fa56f1b85f7757d6f27fafd9f0a993afcc
Haley Kalb <haley.kalb@gmail.com>
2017-05-20 21:16:56 -0600

    OBJECT RENDERING TO PAGE

--- yogapp/yogapp/views.py
+++ yogapp/yogapp/views.py
@@ -1,6 +1,8 @@
-from django.http import JsonResponse
+from django.http import HttpResponse
+from django.core import serializers
 from yoga.models import Users
 
 def guy(request):
     # yogappdb.Users.objects.raw("select profile_picture_img from yoga_users")
-    return JsonResponse(Users.objects.all(), safe=False)
+    data = Users.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
