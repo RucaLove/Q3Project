cf6b31a7cb14ef3f1bca9f9f8b36ef5b4b3cebc4
Haley Kalb <haley.kalb@gmail.com>
2017-05-20 21:38:42 -0600

    returning object to page and rendering

--- yogapp/yogapp/views.py
+++ yogapp/yogapp/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.core import serializers
 from yoga.models import Users
+import psycopg2
+conn = psycopg2.connect("dbname=yogapp user=haleykalb")
+cur = conn.cursor()
+# cur.execute("select profile_picture_img from yoga_users where id=1;")
 
 def guy(request):
-    # yogappdb.Users.objects.raw("select profile_picture_img from yoga_users")
-    data = Users.objects.all()
-    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
+    # data = Users.objects.order_by('id').distinct('profile_picture_img')
+    # select profile_picture_img from yoga_users
+    # data = Users.objects.all()
+    data = cur.execute("select profile_picture_img from yoga_users where id=1;")
+    # return HttpResponse(serializers.serialize('json', data), content_type='application/json')
+    return HttpResponse(data)
